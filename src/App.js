241c2d52fd4ad98fa5ed7ad3c2026248eefffc8e
RaresP27 <petirare@msu.edu>
2023-04-30 14:18:16 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
-import { useState } from "react";
-import './App.css'
-
-function Library() {
-  const [data, setmydata] = useState([]);
-
-  const getAPI = () => {
-    const headers = {'Authorization': "Bearer your-api-key-123"};
-    fetch("https://the-one-api.dev/v2/", { headers })
-    .then((Response) => Response.json())
-    .then((json) => {
-      console.log(json);
-      setmydata(json);
-    })
-  }
-  return (
-    <div>
-      <button onClick={getAPI}>Fetch API</button>
-      {JSON.stringify()}
-    </div>
-  );
-}
+import React, { useState, useEffect } from "react";
+import logo from './ParcsAndRecImage.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+import './App.css';
 
+const MyLogo = [
+  {id: 1, image: logo, width: '658', height: '969'},
+]
 
 function App() {
+  const ParksAndRec = MyLogo.map(parks =>
+    <>
+    <h2 >The Show's Cover:</h2>
+    {parks.image &&
+    <img className="Logo" src= {parks.image} alt = '' style = {{width: parks.width, height: parks.height}}></img>
+    }
+    </>
+  );
+  const [quote, setQuote] = useState([]);
 
+  const fetchData = () => {
+    return fetch("http://ron-swanson-quotes.herokuapp.com/v2/quotes")
+          .then((response) => response.json())
+          .then((data) => setQuote(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+  const [show, setTVShow] = useState([]);
+  const fetchTV = () => {
+    return fetch("https://api.tvmaze.com/lookup/shows?imdb=tt1266020")
+          .then((response) => response.json())
+          .then((data) => setTVShow(data));
+  }
+  useEffect(() => {
+    fetchTV();
+  },[])
+  const [actor, setActors] = useState([]);
+  const fetchActors = () => {
+    return fetch("https://api.tvmaze.com/shows/174/cast")
+          .then((response) => response.json())
+          .then((data) => setActors(data));
+  }
+  useEffect(() => {
+    fetchActors();
+  },[])
+  //fetch method barrowed from https://statusneo.com/different-ways-to-fetch-data-from-api-in-reactjs/
+
+  return (
+   <div className="App">
+      <header className="App-header">
+        <h1 className="Head">
+          Parks and Rec, a Brief Overview
+        </h1>
+        <div className="Cover">{ParksAndRec}</div>
+        <div class="grid text-center" style={{display: "grid"}}>
+          <div class="g-col-4">Summary: <br></br>
+            {show.summary.replace("<p>", ""). replace("</p>", ""). replace("<b>","").replace("</b>","")}
+          </div>
+          <div class="g-col-4"> The Show's Runtime: <br></br>
+            Show began: {show.premiered} <br></br>
+                              Show ended: {show.ended} </div>
+        </div>
+        <div>
+        <p className="Quote">"{quote}" - Ron Swanson (AKA my favorite character)</p></div>
+      </header> </div>
+  );
 }
 
 export default App;
