6ce6ea50e2a72bc2e2b22765e0fa6f9ebf4884cf
RaresP27 <petirare@msu.edu>
2023-04-30 14:27:31 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -49,11 +49,11 @@ function App() {
         <div className="Cover">{ParksAndRec}</div>
         <div class="grid text-center" style={{display: "grid"}}>
           <div class="g-col-4">Summary: <br></br>
-            {show.summary.replace("<p>", ""). replace("</p>", ""). replace("<b>","").replace("</b>","")}
+          {show.summary.replace("<p>", ""). replace("</p>", ""). replace("<b>","").replace("</b>","")}
           </div>
           <div class="g-col-4"> The Show's Runtime: <br></br>
-            Show began: {show.premiered} <br></br>
-                              Show ended: {show.ended} </div>
+          Show began: {show.premiered} <br></br>
+          Show ended: {show.ended} </div>
         </div>
         <div>
         <p className="Quote">"{quote}" - Ron Swanson (AKA my favorite character)</p></div>
