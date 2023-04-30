2a1da407a4b10a99a96647019b2c94ceb245b063
RaresP27 <petirare@msu.edu>
2023-04-30 04:24:53 -0400

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Library() {
+  const [data, setmydata] = useState([]);
+
+  const getAPI = () => {
+    const headers = {'Authorization': "Bearer your-api-key-123"};
+    fetch("https://the-one-api.dev/v2/", { headers })
+    .then((Response) => Response.json())
+    .then((json) => {
+      console.log(json);
+      setmydata(json);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getAPI}>Fetch API</button>
+      {JSON.stringify()}
     </div>
   );
+}
+
+
+
+
+function App() {
+
 }
 
 export default App;
